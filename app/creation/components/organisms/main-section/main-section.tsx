0bd91673f6e6b31afa118d35a306844ab03977d9
julienplatforrm <julienplatforrm@gmail.com>
2025-02-24 21:55:52 +0100

    flow

--- app/creation/components/organisms/main-section/main-section.tsx
+++ app/creation/components/organisms/main-section/main-section.tsx
@@ -1,15 +1,23 @@
 
 // import ResizeableBar from "../../atoms/drag-timeline/drag-timeline";
-import { DateRangeProvider } from "../../../../../src/date-range-context";
+import { DateRangeProvider, useDateRange } from "../../../../../src/date-range-context";
 import DateComponent from "../../molecules/date-component/date-component";
 import ResizeableElements from "../../molecules/resizeable-elements/resizeable-elements";
+import { format, eachDayOfInterval } from "date-fns";
+
+
 const MainSection = () => {
+  const { range, setDateRangeWidth } = useDateRange();
+  // const containerRef = useRef<HTMLDivElement>(null);
+  const dateList = eachDayOfInterval({
+    start: range.startDate,
+    end: range.endDate,
+  });
+
   return (
     <div className="mt-[32px]">
-      <DateRangeProvider>
-        <DateComponent useDate={true} />
-        <ResizeableElements />
-      </DateRangeProvider>
+      <DateComponent useDate={true} />
+      <ResizeableElements dateList={dateList} />
     </div>
   );
 };
