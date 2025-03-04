9d27c0bb1da199b0e32176b9fbd6ae66b768d8dd
julienplatforrm <julienplatforrm@gmail.com>
2025-03-04 16:51:36 +0100

    added field for amount

--- app/creation/components/SetupScreen.tsx
+++ app/creation/components/SetupScreen.tsx
@@ -62,17 +62,15 @@ export const SetupScreen = () => {
   ];
 
   const mediaBudgetPercentage = [
-    { value: "10", label: "10%" },
-    { value: "20", label: "20%" },
-    { value: "30", label: "30%" },
-    { value: "40", label: "40%" },
-    { value: "50", label: "50%" },
-    { value: "60", label: "60%" },
-    { value: "70", label: "70%" },
-    { value: "80", label: "80%" },
-    { value: "90", label: "90%" },
-    { value: "100", label: "100%" },
+    { value: "Fix Budget fee", label: "Fix Budget fee" },
+    { value: "Fix Budget fee", label: "% of media budget" },
+    
   ];
+
+  const currencyPercentage = [
+    {value: "0 %" , label: "0 %"},
+    {value: "0 €  " , label: "0 €"}
+  ]
 
 
 
@@ -118,6 +116,10 @@ export const SetupScreen = () => {
           <div className="flex flex-wrap gap-4">
             <ClientSelection options={selectCurrency} label={"Select currency"} />
             <ClientSelection options={mediaBudgetPercentage} label={"% of media budget"} />
+            <div style={{ width: '100px' }}>
+              <ClientSelection options={currencyPercentage} label={"0 %"} />
+            </div>
+
 
             {/* <Select
               disabled={isEditing}
