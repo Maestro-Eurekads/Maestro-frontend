10ad8410d363c025c46ca16661e5d118c2f2dc19
julienplatforrm <julienplatforrm@gmail.com>
2025-03-06 18:01:54 +0100

    reduce size

--- app/creation/components/ConfiguredSetPage.tsx
+++ app/creation/components/ConfiguredSetPage.tsx
@@ -281,12 +281,12 @@ const ConfiguredSetPage = () => {
 
            <label
   htmlFor="AcceptConditions"
-  className="relative inline-block h-8 w-14 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-blue-500"
+  className="relative inline-block h-6 w-12 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-blue-500"
 >
   <input type="checkbox" id="AcceptConditions" className="peer sr-only" />
 
   <span
-    className="absolute inset-y-0 start-0 m-1 size-6 rounded-full bg-white transition-all peer-checked:start-6"
+    className="absolute inset-y-0 start-0 m-1 size-5 rounded-full bg-white transition-all peer-checked:start-5"
     ></span>
 </label>
 <p className="text-[#061237] text-sm font-semibold overflow-hidden text-ellipsis whitespace-nowrap tracking-tighter">Auto-split budget across ad sets</p>
