07c8349d610c8676968c8ae992f824e37d01f6a5
julienplatforrm <julienplatforrm@gmail.com>
2025-03-14 17:59:36 +0100

    fix

--- app/creation/components/common/Awareness.tsx
+++ app/creation/components/common/Awareness.tsx
@@ -13,25 +13,9 @@ import linkedin from "../../../../public/linkedin.svg";
 import Display from "../../../../public/Display.svg";
 import yahoo from "../../../../public/yahoo.svg";
 import bing from "../../../../public/bing.svg";
-import orangecredit from "../../../../public/orangecredit-card.svg";
-import tablerzoomfilled from "../../../../public/tabler_zoom-filled.svg";
 import tictok from "../../../../public/tictok.svg";
-import cursor from "../../../../public/blue_fluent_cursor-click.svg";
-import State12 from "../../../../public/State12.svg";
-import roundget from "../../../../public/ic_round-get-app.svg";
-import mingcute_basket from "../../../../public/mingcute_basket-fill.svg";
-import mdi_leads from "../../../../public/mdi_leads.svg";
 import AwarenessEdit from "./AwarenessEdit";
 import { useCampaigns } from "../../../utils/CampaignsContext";
-
-const displayMedia = [
-  { id: 1, name: "The TradeDesk", icon: TheTradeDesk },
-  { id: 2, name: "QuantCast", icon: Quantcast },
-  { id: 3, name: "CPV" },
-  { id: 4, name: "CPV" },
-  { id: 5, name: "View view" },
-  { id: 6, name: "Video view" },
-];
 
 const platformIcons = {
   Facebook: facebook,
@@ -57,7 +41,7 @@ const getPlatformIcon = (platformName) => {
 
 const Awareness = ({ edit, onDelete, stageName }) => {
   const { campaignFormData } = useCampaigns();
-  console.log(campaignFormData?.channel_mix);
+
   return (
     <div className="mt-6 bg-gray-100 p-6 gap-8 rounded-lg">
       {/* Header */}
@@ -70,11 +54,29 @@ const Awareness = ({ edit, onDelete, stageName }) => {
 
       {edit ? (
         // Pass the onDelete callback to AwarenessEdit
-        <AwarenessEdit onDelete={onDelete} />
+        <AwarenessEdit
+          onDelete={onDelete}
+          stageName={stageName}
+          sm_data={
+            campaignFormData?.channel_mix?.find(
+              (ch) => ch?.funnel_stage === stageName
+            )["social_media"]
+          }
+          dn_data={
+            campaignFormData?.channel_mix?.find(
+              (ch) => ch?.funnel_stage === stageName
+            )["display_networks"]
+          }
+          se_data={
+            campaignFormData?.channel_mix?.find(
+              (ch) => ch?.funnel_stage === stageName
+            )["search_engines"]
+          }
+        />
       ) : (
-        <div className="mt-6 flex flex-col md:flex-row gap-8 overflow-x-auto">
+        <div className="mt-6 flex flex-col md:flex-row gap-8 overflow-x-auto w-full">
           {/* Social Media Section (Left) */}
-          <div className="shrink-0 w-full md:w-1/2">
+          <div className="shrink-0 w-full md:w-fit">
             <h2 className="text-black font-bold text-md mb-4">Social Media</h2>
             <div className="flex flex-col gap-2">
               <div className="flex items-center gap-4">
@@ -133,112 +135,124 @@ const Awareness = ({ edit, onDelete, stageName }) => {
           </div>
 
           {/* Display Network Section (Right) */}
-          <div className="shrink-0 w-full md:w-1/2">
+          <div className="shrink-0 w-full md:w-fit">
             <h2 className="text-black font-bold text-md mb-4">
-              Display Network
+              Display Networks
             </h2>
-            <div className="grid grid-cols-2 grid-rows-3 gap-4">
-              {campaignFormData?.channel_mix
-                ?.find((ch) => ch?.funnel_stage === stageName)
-                ["display_networks"].map((item) => (
-                  <a
-                    key={item.platform_name}
-                    // onClick={() => window.open(item.link, "_blank")}
-                    className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2"
-                  >
-                    {item.platform_name && (
-                      <Image
-                        src={getPlatformIcon(item.platform_name)}
-                        alt={item.name}
-                        className="size-4"
-                      />
-                    )}
-                    <p className="text-black text-center text-md">
-                      {item.platform_name}
-                    </p>
-                  </a>
-                ))}
-              {campaignFormData?.channel_mix
-                ?.find((ch) => ch?.funnel_stage === stageName)
-                ["display_networks"].map((item) => (
-                  <a
-                    key={item.platform_name}
-                    // onClick={() => window.open(item.link, "_blank")}
-                    className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2"
-                  >
-                    <p className="text-black text-center text-md">
-                      {item.buy_type}
-                    </p>
-                  </a>
-                ))}
-              {campaignFormData?.channel_mix
-                ?.find((ch) => ch?.funnel_stage === stageName)
-                ["display_networks"].map((item) => (
-                  <a
-                    key={item.platform_name}
-                    // onClick={() => window.open(item.link, "_blank")}
-                    className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2"
-                  >
-                    <p className="text-black text-center text-md">
-                      {item.objective_type}
-                    </p>
-                  </a>
-                ))}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-4">
+                {campaignFormData?.channel_mix
+                  ?.find((ch) => ch?.funnel_stage === stageName)
+                  ["display_networks"].map((item) => (
+                    <a
+                      key={item.platform_name}
+                      // onClick={() => window.open(item.link, "_blank")}
+                      className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2 shrink-0 w-[180px]"
+                    >
+                      {item.platform_name && (
+                        <Image
+                          src={getPlatformIcon(item.platform_name)}
+                          alt={item.name}
+                          className="size-4"
+                        />
+                      )}
+                      <p className="text-black text-center text-md">
+                        {item.platform_name}
+                      </p>
+                    </a>
+                  ))}
+              </div>
+              <div className="flex items-center gap-4">
+                {campaignFormData?.channel_mix
+                  ?.find((ch) => ch?.funnel_stage === stageName)
+                  ["display_networks"].map((item) => (
+                    <a
+                      key={item.platform_name}
+                      // onClick={() => window.open(item.link, "_blank")}
+                      className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2 shrink-0 w-[180px]"
+                    >
+                      <p className="text-black text-center text-md">
+                        {item.buy_type}
+                      </p>
+                    </a>
+                  ))}
+              </div>
+              <div className="flex items-center gap-4">
+                {campaignFormData?.channel_mix
+                  ?.find((ch) => ch?.funnel_stage === stageName)
+                  ["display_networks"].map((item) => (
+                    <a
+                      key={item.platform_name}
+                      // onClick={() => window.open(item.link, "_blank")}
+                      className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2 shrink-0 w-[180px]"
+                    >
+                      <p className="text-black text-center text-md">
+                        {item.objective_type}
+                      </p>
+                    </a>
+                  ))}
+              </div>
             </div>
           </div>
 
           {/* Search Engines Section (Right) */}
-          <div className="shrink-0 w-full md:w-1/2">
+          <div className="shrink-0 w-full md:w-fit">
             <h2 className="text-black font-bold text-md mb-4">
               Search Engines
             </h2>
-            <div className="grid grid-cols-2 grid-rows-3 gap-4">
-              {campaignFormData?.channel_mix
-                ?.find((ch) => ch?.funnel_stage === stageName)
-                ["search_engines"].map((item) => (
-                  <a
-                    key={item.platform_name}
-                    // onClick={() => window.open(item.link, "_blank")}
-                    className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2"
-                  >
-                    {item.platform_name && (
-                      <Image
-                        src={getPlatformIcon(item.platform_name)}
-                        alt={item.name}
-                        className="size-4"
-                      />
-                    )}
-                    <p className="text-black text-center text-md">
-                      {item.platform_name}
-                    </p>
-                  </a>
-                ))}
-              {campaignFormData?.channel_mix
-                ?.find((ch) => ch?.funnel_stage === stageName)
-                ["search_engines"].map((item) => (
-                  <a
-                    key={item.platform_name}
-                    // onClick={() => window.open(item.link, "_blank")}
-                    className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2"
-                  >
-                    <p className="text-black text-center text-md">
-                      {item.buy_type}
-                    </p>
-                  </a>
-                ))}
-              {campaignFormData?.channel_mix
-                ?.find((ch) => ch?.funnel_stage === stageName)
-                ["search_engines"].map((item) => (
-                  <a
-                    key={item.platform_name}
-                    // onClick={() => window.open(item.link, "_blank")}
-                    className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2"
-                  >
-                    <p className="text-black text-center text-md">
-                      {item.objective_type}
-                    </p>
-                  </a>
-                ))}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-4">
+                {campaignFormData?.channel_mix
+                  ?.find((ch) => ch?.funnel_stage === stageName)
+                  ["search_engines"].map((item) => (
+                    <a
+                      key={item.platform_name}
+                      // onClick={() => window.open(item.link, "_blank")}
+                      className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2 shrink-0 w-[180px]"
+                    >
+                      {item.platform_name && (
+                        <Image
+                          src={getPlatformIcon(item.platform_name)}
+                          alt={item.name}
+                          className="size-4"
+                        />
+                      )}
+                      <p className="text-black text-center text-md">
+                        {item.platform_name}
+                      </p>
+                    </a>
+                  ))}
+              </div>
+              <div className="flex items-center gap-4">
+                {campaignFormData?.channel_mix
+                  ?.find((ch) => ch?.funnel_stage === stageName)
+                  ["search_engines"].map((item) => (
+                    <a
+                      key={item.platform_name}
+                      // onClick={() => window.open(item.link, "_blank")}
+                      className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2 shrink-0 w-[180px]"
+                    >
+                      <p className="text-black text-center text-md">
+                        {item.buy_type}
+                      </p>
+                    </a>
+                  ))}
+              </div>
+              <div className="flex items-center gap-4">
+                {campaignFormData?.channel_mix
+                  ?.find((ch) => ch?.funnel_stage === stageName)
+                  ["search_engines"].map((item) => (
+                    <a
+                      key={item.platform_name}
+                      // onClick={() => window.open(item.link, "_blank")}
+                      className="flex bg-white px-4 py-3 whitespace-nowrap rounded-md border border-gray-200 items-center gap-2 shrink-0 w-[180px]"
+                    >
+                      <p className="text-black text-center text-md">
+                        {item.objective_type}
+                      </p>
+                    </a>
+                  ))}
+              </div>
             </div>
           </div>
         </div>
--- app/creation/components/common/AwarenessEdit.tsx
+++ app/creation/components/common/AwarenessEdit.tsx
@@ -4,7 +4,7 @@ import Button from "./button";
 import Image from "next/image";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 import trade from "../../../../public/TheTradeDesk.svg";
 import speaker from "../../../../public/mdi_megaphone.svg";
@@ -14,35 +14,31 @@ import instagram from "../../../../public/ig.svg";
 import quantcast from "../../../../public/quantcast.svg";
 import arrowdown from "../../../../public/arrow-down-2.svg";
 import vector from "../../../../public/Vector.svg";
-import { campaignObjectives } from '../../../../components/data';
+import { campaignObjectives } from "../../../../components/data";
 
-const AwarenessEdit = ({ onDelete }) => {
+const AwarenessEdit = ({ onDelete, stageName, sm_data, dn_data, se_data }) => {
   const [isDeleted, setIsDeleted] = useState(false);
   const [dropdowns, setDropdowns] = useState({});
   const [values, setValues] = useState({});
 
   const handleDelete = () => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "You want to delete this stage?",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3175FF',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonColor: "#3175FF",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
         setIsDeleted(true);
         if (onDelete) {
           onDelete();
         }
-        Swal.fire(
-          'Deleted!',
-          'Your stage has been deleted.',
-          'success'
-        )
+        Swal.fire("Deleted!", "Your stage has been deleted.", "success");
       }
-    })
+    });
   };
 
   const handleAddBack = () => {
@@ -50,27 +46,27 @@ const AwarenessEdit = ({ onDelete }) => {
   };
 
   const toggleDropdown = (id) => {
-    setDropdowns(prev => ({
+    setDropdowns((prev) => ({
       ...Object.keys(prev).reduce((acc, key) => ({ ...acc, [key]: false }), {}),
-      [id]: !prev[id]
+      [id]: !prev[id],
     }));
   };
 
   const handleSelect = (id, value) => {
-    setValues(prev => ({
+    setValues((prev) => ({
       ...prev,
-      [id]: value
+      [id]: value,
     }));
-    setDropdowns(prev => ({
+    setDropdowns((prev) => ({
       ...prev,
-      [id]: false
+      [id]: false,
     }));
   };
 
   const renderDropdownButton = (id, defaultValue, options) => {
     return (
       <div className="relative">
-        <button 
+        <button
           onClick={() => toggleDropdown(id)}
           className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] w-[150px] h-[52px] px-4"
         >
@@ -82,7 +78,7 @@ const AwarenessEdit = ({ onDelete }) => {
         {dropdowns[id] && (
           <div className="absolute top-full left-0 w-[150px] bg-white border border-[#0000001A] rounded-[10px] mt-1 z-10">
             {options.map((option, index) => (
-              <button 
+              <button
                 key={index}
                 onClick={() => handleSelect(id, option)}
                 className="w-full text-left px-4 py-2 hover:bg-gray-100"
@@ -113,7 +109,9 @@ const AwarenessEdit = ({ onDelete }) => {
       <div className="flex justify-between w-full">
         <div className="flex items-center gap-4">
           <Image src={speaker} alt="speaker" className="size-5" />
-          <span className="text-lg leading-wider text-[#061237] font-semibold">Awareness</span>
+          <span className="text-lg leading-wider text-[#061237] font-semibold">
+            {stageName}
+          </span>
         </div>
 
         <Button
@@ -128,45 +126,88 @@ const AwarenessEdit = ({ onDelete }) => {
       {/* social media */}
       <div className="flex flex-col items-start gap-4">
         <h2 className="font-bold text-[#061237]">Social Media</h2>
-        <div className="flex justify-center gap-4">
-          <div className="grid grid-cols-3 gap-4">
+        <div className="flex gap-4 w-full">
+          <div className="flex flex-col gap-4 w-[680px] overflow-x-auto">
             {/* First row - Static buttons */}
-            <button className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] w-[150px] h-[52px] px-4">
-              <div className="flex items-center gap-2">
-                <Image src={facebook} className="size-4" alt="facebook" />
-                <span className="text-[#061237] font-semibold whitespace-nowrap">Facebook</span>
+            <div className="flex gap-4 items-center">
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
               </div>
-              <Image src={vector} alt="vector" />
-            </button>
-
-            <button className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] w-[150px] h-[52px] px-4">
-              <div className="flex items-center gap-2">
-                <Image src={instagram} className="size-4" alt="instagram" />
-                <span className="text-[#061237] font-semibold whitespace-nowrap">Instagram</span>
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
               </div>
-              <Image src={vector} alt="vector" />
-            </button>
-
-            <button className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] w-[150px] h-[52px] px-4">
-              <div className="flex items-center gap-2">
-                <Image src={youtube} className="size-4" alt="youtube" />
-                <span className="text-[#061237] font-semibold whitespace-nowrap">Youtube</span>
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
               </div>
-              <Image src={vector} alt="vector" />
-            </button>
-
-            {/* Second row - Dropdown buttons */}
-            {renderDropdownButton('awareness1', 'Awareness', ['Video View', 'Traffic'])}
-            {renderDropdownButton('videoviews1', 'Video views', ['Video View', 'Awareness', 'Traffic'])}
-            {renderDropdownButton('videoviews2', 'Video views', ['Video View', 'Awareness', 'Traffic'])}
-
-            {/* Third row - Dropdown buttons */}
-            {renderDropdownButton('cpm1', 'CPM', ['CPM', 'CPV'])}
-            {renderDropdownButton('cpm2', 'CPM', ['CPM', 'CPV'])}
-            {renderDropdownButton('cpm3', 'CPM', ['CPM', 'CPV'])}
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
+              </div>
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
+              </div>
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
+              </div>
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
+              </div>
+            </div>
+            {/* Second row - Static buttons */}
+            <div className="flex gap-4 items-center">
+              <div className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] h-[52px] px-4 gap-[20px] shrink-0 w-fit">
+                <div className="flex items-center gap-2">
+                  <Image src={facebook} className="size-4" alt="facebook" />
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Facebook
+                  </span>
+                </div>
+                <Image src={vector} alt="vector" />
+              </div>
+            </div>
           </div>
-
-          <div>
+          <div className="shrink-0 max-w-[300px]">
             <button className="w-[153px] h-[52px] bg-[#3175FF] rounded-[8px] border border-[#0000001A] border-solid">
               <span className="text-white">Add new channel</span>
             </button>
@@ -183,7 +224,9 @@ const AwarenessEdit = ({ onDelete }) => {
               <button className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] w-[150px] h-[52px] px-4">
                 <div className="flex items-center gap-2">
                   <Image src={trade} className="size-4" alt="facebook" />
-                  <span className="text-[#061237] font-semibold whitespace-nowrap">TradeDesk</span>
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    TradeDesk
+                  </span>
                 </div>
                 <Image src={vector} alt="vector" />
               </button>
@@ -191,18 +234,28 @@ const AwarenessEdit = ({ onDelete }) => {
               <button className="flex justify-between items-center bg-[#FFFFFF] rounded-[10px] border border-solid border-[#0000001A] w-[150px] h-[52px] px-4">
                 <div className="flex items-center gap-2">
                   <Image src={quantcast} className="size-4" alt="instagram" />
-                  <span className="text-[#061237] font-semibold whitespace-nowrap">Quantcast</span>
+                  <span className="text-[#061237] font-semibold whitespace-nowrap">
+                    Quantcast
+                  </span>
                 </div>
                 <Image src={vector} alt="vector" />
               </button>
 
               {/* Second row - Dropdown buttons */}
-              {renderDropdownButton('videoviews3', 'Video views', ['Video View', 'Awareness', 'Traffic'])}
-              {renderDropdownButton('videoviews4', 'Video views', ['Video View', 'Awareness', 'Traffic'])}
+              {renderDropdownButton("videoviews3", "Video views", [
+                "Video View",
+                "Awareness",
+                "Traffic",
+              ])}
+              {renderDropdownButton("videoviews4", "Video views", [
+                "Video View",
+                "Awareness",
+                "Traffic",
+              ])}
 
               {/* Third row - Dropdown buttons */}
-              {renderDropdownButton('cpm4', 'CPM', ['CPM', 'CPV'])}
-              {renderDropdownButton('cpm5', 'CPM', ['CPM', 'CPV'])}
+              {renderDropdownButton("cpm4", "CPM", ["CPM", "CPV"])}
+              {renderDropdownButton("cpm5", "CPM", ["CPM", "CPV"])}
             </div>
 
             <button className="w-[153px] h-[52px] bg-[#3175FF] rounded-[8px] border border-[#0000001A] border-solid">
