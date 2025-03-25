12188626862d78821ec5f47c65b9b5bb5e79f45a
julienplatforrm <julienplatforrm@gmail.com>
2025-03-25 13:41:52 +0100

    resolve format selection

--- app/creation/components/FormatSelection.tsx
+++ app/creation/components/FormatSelection.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { FaCheck } from "react-icons/fa";
