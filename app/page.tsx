a14c83491da6cf57f0ad39f8b8873b521f543948
julienplatforrm <julienplatforrm@gmail.com>
2025-03-31 18:46:53 +0100

    Revert "fixed login"

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-import { authOptions } from "utils/auth";
 import Homepage from "./homepage/page";
-import { getServerSession } from "next-auth";
-import Login from "features/Login";
-import ClientView from "features/ClientView";
 
-export default async function Home() {
-  const token = await getServerSession(authOptions);
-  console.log("🚀 ~ Home ~ token:", token);
-  if (!token) {
-    return <Login />;
-  }
-  // @ts-ignore
-  return token?.user?.data?.user?.user_type === "admin" ? (
+
+export default function Home() {
+  return (
     <Homepage />
-  ) : (
-    <ClientView />
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./features/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
