1cab3aea54f33f50b2c22b6142dcd88cb8bfaf8e
julienplatforrm <julienplatforrm@gmail.com>
2025-03-31 18:27:22 +0100

    fixed login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import { authOptions } from "utils/auth";
 import Homepage from "./homepage/page";
+import { getServerSession } from "next-auth";
+import Login from "features/Login";
+import ClientView from "features/ClientView";
 
-
-export default function Home() {
-  return (
+export default async function Home() {
+  const token = await getServerSession(authOptions);
+  console.log("🚀 ~ Home ~ token:", token);
+  if (!token) {
+    return <Login />;
+  }
+  // @ts-ignore
+  return token?.user?.data?.user?.user_type === "admin" ? (
     <Homepage />
+  ) : (
+    <ClientView />
   );
 }
