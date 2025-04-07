a26fd4c52de376b24d6659bd700367472ad8cd9f
julienplatforrm <julienplatforrm@gmail.com>
2025-04-07 11:44:04 +0100

    update

--- app/client/ApproverContainer.tsx
+++ app/client/ApproverContainer.tsx
@@ -1,56 +1,37 @@
 import React from 'react'
 
 const ApproverContainer = () => {
+
+	const items = [
+		{ label: "Agency", name: "Eurekads Pte. Ltd.", initials: null },
+		{ label: "Client approver", name: "Chris Kalics", initials: "CK" },
+		{ label: "Agency approver", name: "Julien Dahmoun", initials: "JD" },
+		{ label: "Campaign builder", name: "Maxime Brevet", initials: "MB" },
+	];
+
 	return (
 		<div className='flex flex-col gap-[24px]'>
-			<div className="flex items-center gap-[48px]">
-				<div>
-					<p className="font-medium text-[14px] leading-[19px] text-gray-500">Agency</p>
-					<div className='flex items-center gap-2 mt-2'>
-						<p className="font-medium text-[14px] leading-[19px] text-[#061237]">Eurekads Pte. Ltd.</p>
-					</div>
-				</div>
-				<div>
-					<p className="font-medium text-[14px] leading-[19px] text-gray-500">Client approver</p>
-					<div className='flex items-center gap-2 mt-2'>
-						<div className="flex items-center justify-center p-[6.54545px] gap-[4.36px] w-[24px] h-[24px] bg-[#E8F6FF] rounded-full font-semibold text-[9.42857px] leading-[13px] text-[#3175FF] text-center">
-							CK
+			<div className="flex items-center justify-between">
+				{items.map((item, index) => (
+					<div
+						key={index}
+						className="flex flex-col items-start p-5 gap-2 w-[235px] h-[95px] bg-white shadow-[0px_4px_14px_rgba(0,38,116,0.15)] rounded-[12px]"
+					>
+						<p className="h-[16px] font-[General Sans] font-medium text-[12px] leading-[16px] text-gray-500">
+							{item.label}
+						</p>
+						<div className="flex items-center gap-2 mt-2">
+							{item.initials && (
+								<div className="flex items-center justify-center p-[6.54545px] gap-[4.36px] w-[24px] h-[24px] bg-[#E8F6FF] rounded-full font-semibold text-[9.42857px] leading-[13px] text-[#3175FF] text-center">
+									{item.initials}
+								</div>
+							)}
+							<p className="h-[27px] font-general font-medium text-[20px] leading-[27px] text-[#061237]">
+								{item.name}
+							</p>
 						</div>
-						<p className="font-medium text-[14px] leading-[19px] text-[#061237]">Chris Kalics</p>
 					</div>
-				</div>
-				<div>
-					<p className="font-medium text-[14px] leading-[19px] text-gray-500">Agency approver</p>
-					<div className='flex items-center gap-2 mt-2'>
-						<div className="flex items-center justify-center p-[6.54545px] gap-[4.36px] w-[24px] h-[24px] bg-[#E8F6FF] rounded-full font-semibold text-[9.42857px] leading-[13px] text-[#3175FF] text-center">
-							JD
-						</div>
-						<p className="font-medium text-[14px] leading-[19px] text-[#061237]">Julien Dahmoun</p>
-					</div>
-				</div>
-				<div>
-					<p className="font-medium text-[14px] leading-[19px] text-gray-500">Campaign builder</p>
-					<div className='flex items-center gap-2 mt-2'>
-						<div className="flex items-center justify-center p-[6.54545px] gap-[4.36px] w-[24px] h-[24px] bg-[#E8F6FF] rounded-full font-semibold text-[9.42857px] leading-[13px] text-[#3175FF] text-center">
-							MB
-						</div>
-						<p className="font-medium text-[14px] leading-[19px] text-[#061237]">Maxime Brevet</p>
-					</div>
-				</div>
-				<div>
-					<p className="font-medium text-[14px] leading-[19px] text-gray-500">Focus points</p>
-					<div className='flex items-center gap-2 mt-2'>
-
-						<p className="font-medium text-[14px] leading-[19px] text-[#061237]">12/18</p>
-					</div>
-				</div>
-			</div>
-			<div>
-				<p className="font-medium text-[14px] leading-[19px] text-gray-500">Comment from the agency</p>
-				<div className='flex items-center gap-2 mt-2'>
-
-					<p className="font-medium text-[14px] leading-[19px] text-[#061237]">This plan is optimized for maximizing engagement in the awareness phase. Key adjustments were made based on past performance.</p>
-				</div>
+				))}
 			</div>
 		</div>
 	)
--- app/client/Header.tsx
+++ app/client/Header.tsx
@@ -7,12 +7,10 @@ const Header = ({ setIsOpen }) => {
 
 
   return (
-    <div id="client_header">
+    <div id="client_header" className=" py-[2.8rem] px-[50px] md:px-[100px] xl:px-[300px]">
       <div className="flex flex-col">
         <button
-          className="w-[35px] h-[22px] font-semibold text-[16px] leading-[22px] text-[#061237] font-[General Sans]"
-
-          onClick={() => setIsOpen(true)}  >
+          className="w-[35px] h-[22px] font-semibold text-[16px] leading-[22px] text-[#061237] font-[General Sans]"  >
           Nike
         </button>
         <h1 className="w-[348px] h-[32px] font-semibold text-[24px] leading-[32px] text-[#292929] font-[General Sans]"
@@ -20,7 +18,7 @@ const Header = ({ setIsOpen }) => {
           Spring Collection Launch 2025
         </h1>
       </div>
-      <div
+      {/* <div
         className="text-[18px]"
         onClick={async () =>
           await signOut({
@@ -29,6 +27,15 @@ const Header = ({ setIsOpen }) => {
         }
       >
         Logout
+      </div> */}
+      <div>
+        <button
+          className="bg-[#FAFDFF] text-[16px] font-[600] text-[#3175FF] rounded-[10px] py-[14px] px-6 self-start"
+          style={{ border: "1px solid #3175FF" }}
+          onClick={() => setIsOpen(true)}
+        >
+          Approve & Sign Media plan
+        </button>
       </div>
     </div>
   );
