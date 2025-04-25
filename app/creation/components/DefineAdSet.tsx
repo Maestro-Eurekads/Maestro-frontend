e4d37a634dc9b279c49ab33fd8e4cd92e31b070f
julienplatforrm <julienplatforrm@gmail.com>
2025-04-25 13:11:41 +0100

    move to step 2

--- app/creation/components/DefineAdSet.tsx
+++ app/creation/components/DefineAdSet.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import DefineAdSetPage from './DefineAdSetPage'
 import PageHeaderWrapper from '../../../components/PageHeaderWapper'
-import { useEditing } from '../../utils/EditingContext';
 import { useComments } from 'app/utils/CommentProvider';
 
 const DefineAdSet = () => {
-  const { isEditing, setIsEditing } = useEditing();
   const { setIsDrawerOpen, setClose } = useComments();
   useEffect(() => {
     setIsDrawerOpen(false);
@@ -19,18 +17,7 @@ const DefineAdSet = () => {
           t2={'Specify the details and audiences for each ad set within your campaign.'}
           span={1}
         />
-        {isEditing ? (
-          ''
-        ) : (
-          <button className="model_button_blue" onClick={() => setIsEditing(true)}>
-            Edit
-          </button>
-        )}
       </div>
-
-
-
-
       <DefineAdSetPage />
     </div>
   )
