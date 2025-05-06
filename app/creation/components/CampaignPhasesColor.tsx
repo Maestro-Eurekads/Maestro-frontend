5243b3402b9a17369eb9bbf7ba2e411cc6f918c8
julienplatforrm <julienplatforrm@gmail.com>
2025-05-06 15:52:51 +0100

    update to overview

--- app/creation/components/CampaignPhasesColor.tsx
+++ app/creation/components/CampaignPhasesColor.tsx
@@ -4,7 +4,7 @@ const CampaignPhasesColor = ({ campaignPhases }) => {
 
 
 	return (
-		<div className="campaign_phases_container_two flex flex-row gap-[28px] mt-5">
+		<div className="campaign_phases_container_two flex flex-row flex-wrap  gap-[28px] mt-5">
 			{campaignPhases?.map((phase, index) => (
 				<div key={index} className="flex flex-row items-center   gap-2">
 					<div className={`w-[12px] h-[12px] rounded-[4px] ${phase?.colorClass}`}></div>
