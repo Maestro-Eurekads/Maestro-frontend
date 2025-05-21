4227fe45413921a65c54ea29492b738a8e0ad522
julienplatforrm <julienplatforrm@gmail.com>
2025-05-21 15:48:00 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,15 @@
 import { authOptions } from "utils/auth";
 import Homepage from "./homepage/page";
 import { getServerSession } from "next-auth";
-import Login from "features/Login";
 import ClientView from "./client/ClientView";
 
 export default async function Home() {
   const token = await getServerSession(authOptions);
 
   // @ts-ignore
-  return token?.user?.data?.user?.user_type === "client" || token?.user?.data?.user?.user_type === "client_approver" ? (
-    <ClientView />
-  ) : (
-    <Homepage />
-  );
+  const userType = token?.user?.data?.user?.user_type;
+  const isClient = userType === "client" || userType === "client_approver";
+
+  return isClient ? <ClientView /> : <Homepage />;
 
 }
