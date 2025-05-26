0403237174ff006b3a6e0cc61baae4d62aa1fa77
julienplatforrm <julienplatforrm@gmail.com>
2025-05-26 17:18:52 +0100

    create new plan

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,18 +7,15 @@ import { useCampaigns } from "../app/utils/CampaignsContext";
 import { FiLoader } from "react-icons/fi";
 import useCampaignHook from "../app/utils/useCampaignHook";
 import { useEffect, useState } from "react";
-// Removed unused import 'AllClientsCustomDropdown'
 import { useAppDispatch, useAppSelector } from "store/useStore";
 import AlertMain from "./Alert/AlertMain";
-import { getCreateClient } from "features/Client/clientSlice"; // Removed unused 'reset'
+import { getCreateClient } from "features/Client/clientSlice";
 import { LogOut } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 import { CustomSelect } from "app/homepage/components/CustomReactSelect";
 import { useActive } from "app/utils/ActiveContext";
 import { extractAprroverFilters, extractChannelAndPhase, extractDateFilters, extractLevelFilters, extractLevelNameFilters } from "app/utils/campaign-filter-utils";
 import { useUserPrivileges } from "utils/userPrivileges";
-import { el } from "date-fns/locale";
-import { useSearchParams } from "next/navigation";
 import { getFirstLetters } from "./Options";
 
 
@@ -26,9 +23,6 @@ import { getFirstLetters } from "./Options";
 
 const Header = ({ setIsOpen }) => {
   const { data: session } = useSession();
-  const query = useSearchParams();
-  const campaignId = query.get("campaignId");
-
   if (!session) return null;
   // @ts-ignore 
   const userType = session?.user?.data?.user?.id?.toString() || "";
@@ -144,6 +138,7 @@ const Header = ({ setIsOpen }) => {
 
 
 
+
   return (
     <div id="header" className="relative w-full">
       <div className="flex items-center">
@@ -190,8 +185,8 @@ const Header = ({ setIsOpen }) => {
       <div className="profiledropdown_container_main">
         <div className="profiledropdown_container">
           {(isAdmin ||
-            isFinancialApprover ||
-            isAgencyApprover || isAgencyCreator) &&
+            isFinancialApprover || isAgencyCreator ||
+            isAgencyApprover) &&
             <Link
               href={`/creation`}
               onClick={() => {
