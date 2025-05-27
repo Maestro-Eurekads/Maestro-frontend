109cc744e6f05c138b1cc324a14e3e9ecb769257
julienplatforrm <julienplatforrm@gmail.com>
2025-05-27 14:00:25 +0100

    fix

--- app/homepage/functions/clients.ts
+++ app/homepage/functions/clients.ts
@@ -27,3 +27,22 @@ export const addClientUser = async (userData: any) => {
     }
   );
 };
+
+export const checkExisitingEmails = async (emailList: any) => {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/users`,
+    {
+      params: {
+        filters: {
+          email: {
+            $in: emailList,
+          },
+        },
+      },
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
+      },
+    }
+  );
+  return response.data;
+};
