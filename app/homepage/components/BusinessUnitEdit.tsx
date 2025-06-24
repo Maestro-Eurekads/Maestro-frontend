3aac508093fc277c6ae558455542df60a975e0c3
julienplatforrm <julienplatforrm@gmail.com>
2025-06-24 12:50:57 +0100

    Client Architecture today

--- app/homepage/components/BusinessUnitEdit.tsx
+++ app/homepage/components/BusinessUnitEdit.tsx
@@ -25,11 +25,6 @@ const EditInput = ({ setInputs, label, setAlert, initialData, isAgencyCreator, s
   }, [title, parameters, setInputs]);
 
   const handleAddParameter = () => {
-    if (isAgencyCreator) {
-      toast.error("You do not have permission to perform this action.");
-      return;
-    }
-
     if (
       parameters.length > 0 &&
       !parameters[parameters.length - 1].name.trim()
@@ -46,35 +41,18 @@ const EditInput = ({ setInputs, label, setAlert, initialData, isAgencyCreator, s
   };
 
   const handleRemoveParameter = (index) => {
-    if (isAgencyCreator) {
-      toast.error("You do not have permission to perform this action.");
-      return;
-    }
-    setIsLevelChange(true)
     const updated = [...parameters];
     updated.splice(index, 1);
     setParameters(updated);
   };
 
   const handleParameterChange = (index, value) => {
-    if (isAgencyCreator) {
-      setIsLevelChange(true)
-      toast.error("You do not have permission to perform this action.");
-      return;
-    }
-
     const updated = [...parameters];
     updated[index].name = value;
     setParameters(updated);
   };
 
   const handleAddSubParameter = (index) => {
-    if (isAgencyCreator) {
-      setIsLevelChange(true)
-      toast.error("You do not have permission to perform this action.");
-      return;
-    }
-
     const param = parameters[index];
 
     if (!param.name.trim()) {
@@ -104,23 +82,12 @@ const EditInput = ({ setInputs, label, setAlert, initialData, isAgencyCreator, s
   };
 
   const handleSubChange = (pIndex, sIndex, value) => {
-    if (isAgencyCreator) {
-      setIsLevelChange(true)
-      toast.error("You do not have permission to perform this action.");
-      return;
-    }
-
     const updated = [...parameters];
     updated[pIndex].subParameters[sIndex] = value;
     setParameters(updated);
   };
 
   const handleRemoveSub = (pIndex, sIndex) => {
-    if (isAgencyCreator) {
-      toast.error("You do not have permission to perform this action.");
-      return;
-    }
-
     const updated = [...parameters];
     updated[pIndex].subParameters.splice(sIndex, 1);
     setParameters(updated);
@@ -128,6 +95,7 @@ const EditInput = ({ setInputs, label, setAlert, initialData, isAgencyCreator, s
 
   return (
     <div className="relative w-full">
+      {/* Title input */}
       <label className="font-medium text-[15px] leading-5 text-gray-600">
         {label}
       </label>
@@ -142,57 +110,71 @@ const EditInput = ({ setInputs, label, setAlert, initialData, isAgencyCreator, s
       </div>
 
       {/* Parameters */}
-      {parameters.map((param, index) => (
-        <div key={index} className="mb-4">
-          <div className="mt-3 flex items-center px-4 py-2 h-[40px] border border-[#EFEFEF] rounded-[10px] ml-3">
-            <input
-              type="text"
-              className="w-full bg-transparent outline-none text-gray-600"
-              placeholder={`Add parameter ${index + 1}`}
-              value={param.name}
-              onChange={(e) => handleParameterChange(index, e.target.value)}
-            />
-            <MdOutlineCancel
-              size={18}
-              color="red"
-              onClick={() => handleRemoveParameter(index)}
-              className="cursor-pointer"
-            />
-          </div>
+      {/* Wrapper for hierarchy line from parent */}
+      <div className="relative mt-2 ml-4 border-l-2 border-dashed border-[#3175FF] pl-4">
+        {parameters.map((param, index) => (
+          <div key={index} className="mb-4 relative">
 
-          {param.subParameters.map((sub, sIndex) => (
-            <div
-              key={sIndex}
-              className="ml-6 mt-2 flex items-center px-4 py-2 h-[40px] border border-[#EFEFEF] rounded-[10px]"
-            >
+            {/* Horizontal connector from main line to parameter */}
+            <div className="absolute left-[-16px] top-[20px] w-4 h-0.5 bg-[#3175FF]" />
+
+            {/* Parameter input */}
+            <div className="flex items-center px-4 py-2 h-[40px] border border-[#EFEFEF] rounded-[10px] bg-white">
               <input
                 type="text"
                 className="w-full bg-transparent outline-none text-gray-600"
-                placeholder={`Add sub - parameter ${sIndex + 1}`}
-                value={sub}
-                onChange={(e) =>
-                  handleSubChange(index, sIndex, e.target.value)
-                }
+                placeholder={`Add parameter ${index + 1}`}
+                value={param.name}
+                onChange={(e) => handleParameterChange(index, e.target.value)}
               />
               <MdOutlineCancel
                 size={18}
                 color="red"
-                onClick={() => handleRemoveSub(index, sIndex)}
+                onClick={() => handleRemoveParameter(index)}
                 className="cursor-pointer"
               />
             </div>
-          ))}
 
-          <button
-            onClick={() => handleAddSubParameter(index)}
-            className="ml-4 mt-2 flex items-center gap-1 text-[#3175FF] font-semibold text-[14px]"
-          >
-            <Image src={blueSmallPlue} alt="add" />
-            Add sub-parameter {param.subParameters.length + 1}
-          </button>
-        </div>
-      ))}
+            {/* Sub-parameters with extended vertical line */}
+            <div className="relative ml-6 mt-2 pl-4 before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-px before:bg-[#3175FF] before:translate-x-[-1px]">
+              {param.subParameters.map((sub, sIndex) => (
+                <div
+                  key={sIndex}
+                  className="relative flex items-center h-[45px] mb-2 px-4 border border-[#EFEFEF] rounded-[10px] bg-white before:content-[''] before:absolute before:left-[-16px] before:top-1/2 before:w-4 before:h-0.5 before:bg-[#3175FF]"
+                >
+                  <input
+                    type="text"
+                    className="w-full bg-transparent outline-none text-gray-600"
+                    placeholder={`Add sub-parameter ${sIndex + 1}`}
+                    value={sub}
+                    onChange={(e) =>
+                      handleSubChange(index, sIndex, e.target.value)
+                    }
+                  />
+                  <MdOutlineCancel
+                    size={18}
+                    color="red"
+                    onClick={() => handleRemoveSub(index, sIndex)}
+                    className="cursor-pointer"
+                  />
+                </div>
+              ))}
+            </div>
 
+            {/* Add sub-parameter button */}
+            <button
+              onClick={() => handleAddSubParameter(index)}
+              className="ml-6 mt-2 flex items-center gap-1 text-[#3175FF] font-semibold text-[14px]"
+            >
+              <Image src={blueSmallPlue} alt="add" />
+              Add sub-parameter {param.subParameters.length + 1}
+            </button>
+          </div>
+        ))}
+      </div>
+
+
+      {/* Add parameter button */}
       <div className="flex items-center gap-2 mt-3 ml-1">
         <button
           onClick={handleAddParameter}
