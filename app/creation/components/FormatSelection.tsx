f957d4739cc156576d8b4337def9cd471b6376eb
girgisnabil-cofo <girgis.nabil@coformatique.com>
2025-12-18 16:21:04 +0200

    fix : removing cursor pointer from the whole section in step 2 and 3 (#29)

--- app/creation/components/FormatSelection.tsx
+++ app/creation/components/FormatSelection.tsx
@@ -1658,7 +1658,7 @@ export const FormatSelection = ({
           t2="Select the creative formats you want to use for your campaign. Specify the number of visuals for each format. Multiple formats can be selected per channel or Ad set"
         />
       )}
-      <div className="mt-[32px] flex flex-col gap-[24px] cursor-pointer">
+      <div className="mt-[32px] flex flex-col gap-[24px]">
         {!stageName && (
           <div className="flex justify-center gap-3">
             <p className="font-medium">Channel Granularity</p>
@@ -1692,7 +1692,7 @@ export const FormatSelection = ({
             return (
               <div key={index}>
                 <div
-                  className={`flex justify-between items-center p-6 gap-3 w-full h-[72px] bg-[#FCFCFC] border border-[rgba(0,0,0,0.1)] ${isOpen ? "rounded-t-[10px]" : "rounded-[10px]"
+                  className={`flex justify-between items-center p-6 gap-3 w-full h-[72px] bg-[#FCFCFC] border border-[rgba(0,0,0,0.1)] cursor-pointer ${isOpen ? "rounded-t-[10px]" : "rounded-[10px]"
                     }`}
                   onClick={() => toggleTab(stage.name)}
                 >
